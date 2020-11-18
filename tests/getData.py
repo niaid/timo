77d9f6f3ba452745c143734c90fcd7f6cd21dd01
Dave Chen <dchen@mail.nih.gov>
2020-11-18 14:11:10 -0500

    checkin of getData.py

---
+++ tests/getData.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+import os
+import subprocess
+
+
+def getData():
+
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    root_dir = os.path.dirname(script_dir)
+
+    data_files = [
+        [ 'CV_19_fixed.cov19.rmd.merged.bam', '5fb5412e50a41e3d195dc035' ]
+    ]
+
+
+    data_dir = root_dir + '/Data/'
+
+    url = 'https://data.kitware.com/api/v1/file/'
+
+    for x in data_files:
+        name = x[0]
+        hash_id = x[1]
+        print("")
+        print(name, hash_id)
+
+        full_path =  data_dir + name
+        if os.path.isfile(full_path):
+            print(full_path, "exists")
+        else:
+            params = [ 'curl', '-o', full_path, url + hash_id + '/download' ]
+            print(params)
+            subprocess.run(params)
+
+    print("")
+
+if __name__ == '__main__':
+    getData()
