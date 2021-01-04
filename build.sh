40b7d9cb7990ae8391637fcd450e8ac4a34be8f2
Dave Chen <dchen@mail.nih.gov>
2021-01-04 13:05:31 -0500

    added pwd variable

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-#! /bin/sh
+#! /bin/bash
 
-python setup.py build_ext --inplace
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+python $DIR/setup.py build_ext --inplace
 
