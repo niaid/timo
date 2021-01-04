a10eec8fef66cf330e1400dd6c8ba25e1790f2f1
Dave Chen <dchen@mail.nih.gov>
2021-01-04 12:27:02 -0500

    added -O3 flag

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import os
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(ext_modules = cythonize('analyzer.pyx'))
+os.environ['CFLAGS'] = '-O3 -Wall'
+
+setup(
+     ext_modules = cythonize('analyzer.pyx')
+     )
